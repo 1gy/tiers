bc647d6e7d6d75dc34b3878ff46cb140d6bbabae
1gy <42576495+1gy@users.noreply.github.com>
2023-08-19 05:34:03 +0000

    rewrite Appbar

--- tiers/src/components/presentational/Appbar.tsx
+++ tiers/src/components/presentational/Appbar.tsx
@@ -1,14 +1,28 @@
 import type { FC, ReactNode } from "react";
-import { MuiAppBar, MuiToolbar } from "./Mui";
+import { css } from "../../../styled-system/css";
 
 export type AppbarProps = {
 	children?: ReactNode;
 };
 
 export const Appbar: FC<AppbarProps> = ({ children }) => (
-	<MuiAppBar position="static">
-		<MuiToolbar variant="dense" disableGutters sx={{ ml: 2 }}>
+	<header
+		className={css({
+			backgroundColor: "app.primary",
+			color: "white",
+			display: "flex",
+		})}
+	>
+		<div
+			className={css({
+				minH: "12",
+				display: "flex",
+				alignItems: "center",
+				pl: "4",
+				w: "full",
+			})}
+		>
 			{children}
-		</MuiToolbar>
-	</MuiAppBar>
+		</div>
+	</header>
 );
