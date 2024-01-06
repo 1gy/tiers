ed5689c1c2e6037e1c05e2c15edf6f54e23eca68
1gy <42576495+1gy@users.noreply.github.com>
2024-01-06 09:52:10 +0000

    fix route

--- tiers/src/pages/Routes.tsx
+++ tiers/src/pages/Routes.tsx
@@ -1,23 +1,14 @@
 import type { FC } from "react";
-import Rocon, { useRoutes, Redirect } from "rocon/react";
+import Rocon, { useRoutes } from "rocon/react";
 import { ErrorBoundary } from "../components/functional/ErrorBoundary";
 import { ErrorFallback } from "../components/presentational/ErrorFallback";
 import { TierEditorPage } from "./TierEditor";
 import { TierListPage } from "./TierEditor/TierListPage";
 
-export const IndexPage: FC = () => {
-	return <Redirect route={routes._.edit} />;
-};
-
 export const routes = Rocon.Root({ root: { pathname: "/tiers", state: null } })
 	.attach(Rocon.Path())
-	.exact({ action: () => <IndexPage /> })
-	.route("edit", (route) => route.action(() => <TierListPage />));
-
-export const editorRoutes = routes._.edit
-	.attach(Rocon.Path().any("id"))
-	.anyRoute.attach(Rocon.Path())
-	.exact({ action: ({ id }) => <TierEditorPage defKey={id} /> });
+	.exact({ action: () => <TierListPage /> })
+	.any("id", { action: ({ id }) => <TierEditorPage defKey={id} /> });
 
 export const Routes: FC = () => {
 	return (
