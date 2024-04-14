d0e0cc290d9f0cdc21fba31e1f6c3f14bf4d322b
1gy <42576495+1gy@users.noreply.github.com>
2024-04-14 14:18:31 +0000

    localStorageで設定を保存できるようにする

--- tiers2/src/features/settings-dialog/@settings-dialog.tsx
+++ tiers2/src/features/settings-dialog/@settings-dialog.tsx
@@ -1,20 +1,35 @@
+import { css } from "@styled-system/css";
 import type { FunctionalComponent } from "preact";
-import { Button } from "../ui";
+import { Button, Slider } from "../ui";
 import {
 	Dialog,
 	DialogActions,
 	DialogContent,
 	DialogTitle,
 } from "../ui/dialog";
-import { useSettingsDialog } from "./settings-dialog.usecase";
+import {
+	useCardSize,
+	useSetCardSize,
+	useSettingsDialog,
+} from "./settings-dialog.usecase";
 
 export const SettingsDialog: FunctionalComponent = () => {
 	const { isOpen, close } = useSettingsDialog();
+	const cardSize = useCardSize();
+	const setCardSize = useSetCardSize();
 
 	return (
 		<Dialog open={isOpen} onClose={close}>
 			<DialogTitle>設定</DialogTitle>
-			<DialogContent>aa</DialogContent>
+			<DialogContent>
+				<Slider
+					min={32}
+					max={128}
+					value={cardSize}
+					onChange={(v) => setCardSize(v)}
+					className={css({ w: "full" })}
+				/>
+			</DialogContent>
 			<DialogActions>
 				<Button onClick={close}>閉じる</Button>
 			</DialogActions>
--- tiers2/src/features/settings-dialog/settings-dialog.usecase.ts
+++ tiers2/src/features/settings-dialog/settings-dialog.usecase.ts
@@ -1,4 +1,5 @@
-import { atom, useAtom } from "jotai";
+import { atom, useAtom, useSetAtom } from "jotai";
+import { atomWithStorage } from "jotai/utils";
 import { useCallback } from "preact/hooks";
 
 const openAtom = atom(false);
@@ -9,3 +10,9 @@ export const useSettingsDialog = () => {
 	const close = useCallback(() => setIsOpen(false), []);
 	return { isOpen, open, close };
 };
+
+const cardSizeAtom = atomWithStorage("tiers2/settings/cardSize", 76);
+
+export const useCardSize = () => useAtom(cardSizeAtom)[0];
+
+export const useSetCardSize = () => useSetAtom(cardSizeAtom);
