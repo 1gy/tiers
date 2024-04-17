7fa4fe2c128bb89f211e3499c7de2194d92c4c54
1gy <42576495+1gy@users.noreply.github.com>
2024-04-17 13:34:57 +0000

    シーズンリストからの移動を実装

--- tiers2/src/features/seasons/@season-list.tsx
+++ tiers2/src/features/seasons/@season-list.tsx
@@ -1,18 +1,25 @@
 import type { FunctionComponent } from "preact";
+import { useLocation } from "wouter-preact";
 import { List, ListItem, ListItemButton, ListItemText } from "../ui";
 
+const items = [
+	{ label: "2023年 冬アニメ", url: "/seasons/2023-WINTER" },
+	{ label: "2023年 春アニメ", url: "/seasons/2023-SPRING" },
+	{ label: "2023年 夏アニメ", url: "/seasons/2023-SUMMER" },
+	{ label: "2023年 秋アニメ", url: "/seasons/2023-FALL" },
+	{ label: "2024年 冬アニメ", url: "/seasons/2024-WINTER" },
+	{ label: "2024年 春アニメ", url: "/seasons/2024-SPRING" },
+];
+
 export const SeasonList: FunctionComponent = () => {
+	const [_, navigate] = useLocation();
+
 	return (
 		<List>
-			<ListItem>
-				<ListItemButton>
-					<ListItemText>Item 1</ListItemText>
-				</ListItemButton>
-			</ListItem>
-			{[...Array(10).keys()].map((i) => (
-				<ListItem key={i}>
-					<ListItemButton>
-						<ListItemText>Item {i + 2}</ListItemText>
+			{items.map((item) => (
+				<ListItem key={item.url}>
+					<ListItemButton onClick={() => navigate(item.url)}>
+						<ListItemText>{item.label}</ListItemText>
 					</ListItemButton>
 				</ListItem>
 			))}
