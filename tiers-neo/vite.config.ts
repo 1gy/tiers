76af0566de6b47275bb6dc766a45f59f023e4a15
1gy <42576495+1gy@users.noreply.github.com>
2025-01-11 14:30:07 +0000

    vitestを導入する

--- tiers-neo/vite.config.ts
+++ tiers-neo/vite.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig } from "vite";
+import { defineConfig, mergeConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
+
 import react from "@vitejs/plugin-react-swc";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
+const viteConfig = defineConfig({
 	base: "/tiers/",
 	plugins: [
 		TanStackRouterVite(),
@@ -13,3 +15,14 @@ export default defineConfig({
 		host: true,
 	},
 });
+
+const vitestConfig = defineVitestConfig({
+	test: {
+		includeSource: ["src/**/*.{ts,tsx}"],
+	},
+	define: {
+		"import.meta.vitest": "undefined",
+	}
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
